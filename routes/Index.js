9690d19f759aea1c02af2b3bd406893ac96ad7d7
Ted Ung <tuanvan424@gmail.com>
2022-06-08 09:04:27 +0700

    add product api

--- routes/Index.js
+++ routes/Index.js
@@ -1,14 +1,17 @@
+
 const categoryRouter = require("./Category");
 const userRouter = require("./User");
 const storeRouter = require("./Store");
 const orderRouter = require("./Order");
 const uploadFileRoute = require("./UploadFile");
 const authRouter = require("./Auth");
+const productRouter = require('./Product');
 
 function route(app) {
   app.use(categoryRouter);
   app.use(userRouter);
   app.use(storeRouter);
+  app.use(productRouter);
   app.use(orderRouter);
   app.use(uploadFileRoute);
   app.use(authRouter);
